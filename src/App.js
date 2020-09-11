3c8be188e8b34df47c5fad69bd85cd773a6355d4
ievavizg <ievaviz92@gmail.com>
2020-09-11 09:28:18 +0300

    fixed to build

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import CurrencyExhange from "./components/CurrencyExchange";
-import Title from "./components/Title";
 
 const App = () => {
 
         return (
+
             <div     style={{
                 position: 'absolute', left: '50%', top: '50%',
                 transform: 'translate(-50%, -50%)'
             }}>
-                <Title/>
                 <div>
                     Currency Exchanger
                 </div>
